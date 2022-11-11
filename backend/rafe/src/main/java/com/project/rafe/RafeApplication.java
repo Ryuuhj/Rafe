45543e0ae4f60858fa709199037c085a48031780
Ryuuhj <hre1999@sookmyung.ac.kr>
2022-11-12 00:56:52 +0900

    initial commit

---
+++ backend/rafe/src/main/java/com/project/rafe/RafeApplication.java
@@ -0,0 +1,13 @@
+package com.project.rafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RafeApplication.class, args);
+	}
+
+}
