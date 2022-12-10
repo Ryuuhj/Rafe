0881e33e7c1c073e14ae2eb941bce7818c0b4a13
snghx <nho0814@sookmyung.ac.kr>
2022-12-10 20:27:36 +0900

    fix : 최저가 부분 수정

--- frontend/src/pages/IngredientDetailPage.js
+++ frontend/src/pages/IngredientDetailPage.js
@@ -20,7 +20,7 @@ function IngredientDetailPage() {
             igId: ingredient_id
         })
             .then(res => {
-                //console.log(res.data.recipes)
+                //console.log(res.data)
                 setIngreList(res.data);
             })
     }, [])
@@ -61,12 +61,14 @@ function IngredientDetailPage() {
                 <div className="MinPrice__tot">
                     <div id="MinPrice__title">{ingreList.igName} 최저가</div>
 
-                    {ingreList.itmes
-                        ? (<table> {ingreList.items.map((val) => {
+                    {ingreList.items
+                        ? (<table className="MinPrice__table"> {ingreList.items.map((val) => {
+                            val.title = val.title.replace('<b>', " ")
+                            val.title = val.title.replace('</b>', " ")
                             return (
                                 <tr>
                                     <td className="MinPrice__img">
-                                        <img src={val.image} alt={"상품 이미지가 없습니다."} style={{ width: '20%' }} />
+                                        <img src={val.image} alt={"상품 이미지가 없습니다."} style={{ width: '60%' }} />
                                     </td>
                                     <td className="MinPrice__name">
                                         {val.title}
@@ -75,7 +77,7 @@ function IngredientDetailPage() {
                                         {val.lprice} 원
                                     </td>
                                     <td className="MinPrice__link">
-                                        <Btn context={"구매하기"} orange={false}
+                                        <Btn context={"구매"} orange={false}
                                             onClick={() => window.open(`${val.link}`, "_blank")} />
                                     </td>
                                 </tr>
