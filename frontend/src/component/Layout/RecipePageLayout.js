805d45b76e945939f6cc15348762d68fb530b2b9
snghx <nho0814@sookmyung.ac.kr>
2023-03-06 18:41:25 +0900

    fix:페이지네이션 수정

--- frontend/src/component/Layout/RecipePageLayout.js
+++ frontend/src/component/Layout/RecipePageLayout.js
@@ -1,6 +1,9 @@
 import "./Text.css";
 import "./css/RecipePageLayout.css";
-import Pagination from "./Pagination";
+//import Pagination from "./Pagination";
+
+import './css/Pagination.css';
+import Pagination from "react-js-pagination";
 
 import { useState } from "react";
 import { Link } from "react-router-dom";
@@ -9,6 +12,10 @@ function RecipePageLayout({ recipe, length }) {
     const [limit, setLimit] = useState(12);
     const [page, setPage] = useState(1);
     const offset = (page - 1) * limit;
+
+    const handlePageChange = (page) => {
+        setPage(page);
+      };
 
     return (
         <div className="recipeBox">
@@ -23,12 +30,23 @@ function RecipePageLayout({ recipe, length }) {
                 </div>
             ))}
             <footer className="footer">
-                <Pagination
+                {/* <Pagination
                     total={length}
                     limit={limit}
                     page={page}
                     setPage={setPage}
                 />
+                 */}
+                 <Pagination
+      activePage={page} // 현재 페이지
+      itemsCountPerPage={12} // 한 페이지랑 보여줄 아이템 갯수
+      totalItemsCount={length} // 총 아이템 갯수
+      pageRangeDisplayed={5} // paginator의 페이지 범위
+      prevPageText={"‹"} // "이전"을 나타낼 텍스트
+      nextPageText={"›"} // "다음"을 나타낼 텍스트
+      onChange={handlePageChange} // 페이지 변경을 핸들링하는 함수
+      
+    />
             </footer>
         </div>
     );
